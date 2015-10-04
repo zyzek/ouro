268d40d26caece038d65c5bfb34d3711eba5990f
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2015-10-04 21:38:50 +1100

    The last of the commenting and cleanup for now. Going to close #9

--- Imp/Source/Interpreter.hs
+++ Imp/Source/Interpreter.hs
@@ -7,19 +7,31 @@ import Data.List
 import Debug.Trace
 
 
+-- | The execution environment, containing the current values of registers and variables.
 data Env = Env { eReg :: [(Reg, Int)], eVar :: [(Id, Int)] } deriving Show
+
+-- | A new empty environment.
 newEnv :: Env
 newEnv = Env [] []
 
-data StepArgs = StepArgs { 
+
+-- | A structure passed into the step function, which contains:
+-- |  An environment;
+-- |  A list of functions defined in the program, for looking up a function call;
+-- |  The current function being executed, used by ouroboroi;
+-- |  The current block, for obtaining the next instruction to execute;
+-- |  The register in which to store the return value of the current function call.
+data StepArgs 
+ = StepArgs { 
     sEnv :: Env,
     sFuncs :: [Function],
     sFunc :: Function,
     sBlock :: Block,
     sReg :: Reg
-} deriving Show
+ } deriving Show
 
 
+-- | Utilities for updating the fields of a StepArgs structure. 
 setEnv :: StepArgs -> Env -> StepArgs
 setEnv sArgs env = sArgs { sEnv = env }
 
@@ -30,6 +42,7 @@ setBlock :: StepArgs -> Block -> StepArgs
 setBlock sArgs block = sArgs { sBlock = block }
 
 
+-- | Function field extraction.
 fId :: Function -> Id
 fId (Function funcId _ _) = funcId
 
@@ -40,6 +53,7 @@ fBlocks :: Function -> [Block]
 fBlocks (Function _ _ funcBlocks) = funcBlocks
 
 
+-- | Block field extraction.
 bId :: Block -> Int
 bId (Block blkId _) = blkId
 
@@ -47,7 +61,7 @@ bInstrs :: Block -> [Instr]
 bInstrs (Block _ blkInstrs) = blkInstrs
 
 
- -- Tools!!
+-- | Environment-manipulation tools!!
 setReg :: Env -> Reg -> Int -> Env
 setReg (Env reg var) targetReg val
  = Env (addToAL reg targetReg val) var
@@ -76,11 +90,14 @@ storeReg :: Env -> Id -> Reg -> Env
 storeReg env varId src
  = setVar env varId (getReg env src)
 
+
+-- | Given an integer, produce that many consecutive registers labeled from n to 1.
 makeNRegs :: Int -> [Reg]
 makeNRegs
  = unfoldr (\b -> if b == 0 then Nothing else Just (Reg b, b - 1))
 
- 
+
+-- | Call the main function of the given program with the given arguments. Return overall result.
 startProgram :: Program -> [Int] -> Int
 startProgram (Program funcList) args
  = let func    = lookupFunc (Id "main") funcList
@@ -90,12 +107,20 @@ startProgram (Program funcList) args
    in  getReg env (Reg 0)
 
 
+-- | Call a function. The args are as follows:
+-- |  The input environment;
+-- |  The register into which the called function should place the result;
+-- |  The list of functions defined in the program;
+-- |  The function to call;
+-- |  The list of registers holding the function arguments.
+-- | Return the environment with the result placed in the correct register.
 call :: Env -> Reg -> [Function] -> Function -> [Reg] -> Env
 call env gReg funcs func argRegs
  = let argVals = map (getReg env) argRegs
    in  setReg env gReg (evalFunc funcs func argVals gReg)
 
 
+-- | Call a function with integer arguments and return the integer result.
 evalFunc :: [Function] -> Function -> [Int] -> Reg -> Int
 evalFunc funcs func@(Function _ argIds (block:_)) argVals gReg
  = let args = zip argIds argVals
@@ -104,6 +129,13 @@ evalFunc funcs func@(Function _ argIds (block:_)) argVals gReg
 evalFunc _ _ _ _ = 0
     
 
+-- | Advance the program a single execution step, return the resulting environment.
+--
+-- | Retrieve the next instruction from the current block,
+-- |  update the current environment by applying that instruction,
+-- |  return the result of calling step on the updated environment, minus the run instruction.
+-- |
+-- | If the current block is empty, advance to the next block.
 step :: StepArgs -> Env
 step sArgs@(StepArgs _ _ func (Block blockId []) _)
  = step $ setBlock sArgs $ lookupBlock (blockId + 1) (fBlocks func)
@@ -126,12 +158,14 @@ step sArgs@(StepArgs env funcs func (Block blockId (i:is)) gReg)
                                            (unwords (map (show . getReg env) pRegs))
                                            (step newArgs)
 
- 
+
+-- | Given a list of functions, return the one with the given Id.
 lookupFunc :: Id -> [Function] -> Function
 lookupFunc wantId funcs 
  = head $ filter (\(Function haveId _ _) -> wantId == haveId) funcs
 
 
+-- | Given a list of blocks, return the one with the given Int as its id.
 lookupBlock :: Int -> [Block] -> Block
 lookupBlock wantId bs
  = head $ filter (\(Block haveId _) -> wantId == haveId) bs
@@ -165,6 +199,7 @@ arithCalc op op1 op2
         
         OpNot   -> if op1 /= 0 then 0 else 1
         OpNeg   -> -op1
+
 
 -- | Apply an operator to the contents of two registers, store the result back in the environment.
 arithOp :: Env -> OpArith -> Reg -> Reg -> Reg -> Env
